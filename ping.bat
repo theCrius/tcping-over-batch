e60b71a903fad6e0992e690586e47847254b1fcd
Claudio Vallesi <claudio.vallesi@gmail.com>
2017-12-25 22:58:59 +0000

    Change format of output to ddmmyyyy_hhmmss_host
    Assuming the system format comes into dd-mm-yyyy and time hh:mm:ss
    
    American friends, edit the file to adjust!

--- ping.bat
+++ ping.bat
@@ -1,3 +1,8 @@
 @echo off
 set /p host=host Address: 
-tcping.exe -j -t -d --tee Log_%host%.log %host%
+
+set "d=%date:~0,2%%date:~3,2%%date:~6,4%"
+set "h=%time:~0,2%%time:~3,2%%time:~6,2%" 
+set "filename=%d%_%h%_%host%.log" 
+
+tcping.exe -j -t -d --tee %filename% %host%
