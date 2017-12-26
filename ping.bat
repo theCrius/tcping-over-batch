e47e746564b098053a894c905df4d0c9b82c57a2
Claudio Vallesi <claudio.vallesi@gmail.com>
2017-12-26 02:18:14 +0000

    Time is always a bitch
    Single digit hours. Really windows?

--- ping.bat
+++ ping.bat
@@ -1,8 +1,11 @@
 @echo off
-set /p host=host Address: 
 
 set "d=%date:~0,2%%date:~3,2%%date:~6,4%"
-set "h=%time:~0,2%%time:~3,2%%time:~6,2%" 
-set "filename=%d%_%h%_%host%.log" 
+SET "hours=%time:~0,2%"
+IF "%hours:~0,1%" == " " SET hours=0%hours:~1,1%
+set "t=%hours%%time:~3,2%%time:~6,2%"
+
+set /p host=host Address: 
+set "filename=%d%_%t%_%host%.log"
 
 tcping.exe -j -t -d --tee %filename% %host%
