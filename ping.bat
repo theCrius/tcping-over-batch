77b79d4998b72b80ea2e0d02df7a21d19eca407f
Claudio Vallesi <claudio.vallesi@gmail.com>
2017-12-29 11:56:39 +0000

    Cleaning up

--- ping.bat
+++ ping.bat
@@ -1,8 +1,8 @@
 @echo off
 
 set "d=%date:~0,2%%date:~3,2%%date:~6,4%"
-SET "hours=%time:~0,2%"
-IF "%hours:~0,1%" == " " SET hours=0%hours:~1,1%
+set "hours=%time:~0,2%"
+if "%hours:~0,1%" == " " set hours=0%hours:~1,1%
 set "t=%hours%%time:~3,2%%time:~6,2%"
 
 set /p host=host Address: 
