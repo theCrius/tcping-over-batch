181271a55b2ebd69ddc7e25140ade6c7396fa4d9
Claudio Vallesi <claudio.vallesi@gmail.com>
2021-07-26 17:44:15 +0100

    Fix bug with log generation

--- ping.bat
+++ ping.bat
@@ -1,15 +1,15 @@
 @echo off
 
-SET "d=%date:~0,2%%date:~3,2%%date:~6,4%"
-	SET "hours=%time:~0,2%"
-	IF "%hours:~0,1%" == " " (
-		SET hours=0%hours:~1,1% 
-	)
-	SET "t=%hours%%time:~3,2%%time:~6,2%"
-	SET "filename=%d%_%t%_%host%.log"
-
 SET /p host=host Address: 
 SET /p toLog=Create a log (y/n): 
+
+SET "d=%date:~0,2%%date:~3,2%%date:~6,4%"
+SET "hours=%time:~0,2%"
+IF "%hours:~0,1%" == " " (
+	SET hours=0%hours:~1,1% 
+)
+SET "t=%hours%%time:~3,2%%time:~6,2%"
+SET "filename=%d%_%t%_%host%.log"
 
 IF "%toLog%" == "y" (
 	tcping.exe -j -t -d --tee %filename% %host% 
